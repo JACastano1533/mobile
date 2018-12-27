cace5f6a00d3695682173682155609a3bcd8e45e
mdmanouwer Hussain <mdmanouwer@gmail.com>
2018-12-27 15:59:32 +0530

    shipping date calculation

---
+++ sites/all/modules/custom/uc_custom_fedex/vendor/florianv/business/src/Day.php
@@ -0,0 +1,36 @@
+<?php
+
+/*
+ * This file is part of Business.
+ *
+ * (c) Florian Voutzinos <florian@voutzinos.com>
+ *
+ * For the full copyright and license information, please view the LICENSE
+ * file that was distributed with this source code.
+ */
+
+namespace Business;
+
+/**
+ * Represents a standard business day.
+ *
+ * @author Florian Voutzinos <florian@voutzinos.com>
+ */
+final class Day extends AbstractDay implements \Serializable
+{
+    /**
+     * {@inheritdoc}
+     */
+    public function serialize()
+    {
+        return serialize([$this->dayOfWeek, $this->openingIntervals]);
+    }
+
+    /**
+     * {@inheritdoc}
+     */
+    public function unserialize($serialized)
+    {
+        list($this->dayOfWeek, $this->openingIntervals) = unserialize($serialized);
+    }
+}
